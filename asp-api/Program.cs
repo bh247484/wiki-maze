c0e994cffea0afa91de087d5acddec6b7a9d7456
bh247484 <bh247484@gmail.com>
2023-05-27 09:38:33 -0400

    Add cors config to asp backend

--- asp-api/Program.cs
+++ asp-api/Program.cs
@@ -2,6 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+
+  options.AddPolicy("CorsPolicy", policy =>
+    {
+      policy
+        .WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
