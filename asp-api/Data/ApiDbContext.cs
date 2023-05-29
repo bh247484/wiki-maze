958c93cc1e779db5863fa6537a772ec21eed003e
bh247484 <bh247484@gmail.com>
2023-05-29 19:56:52 -0400

    Standup Test model and db context

---
+++ asp-api/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+using asp_net_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_net_app.Data;
+
+public class ApiDbContext : DbContext {
+
+  public ApiDbContext(DbContextOptions<ApiDbContext> options)
+    : base(options)
+  {
+
+  }
+
+  public DbSet<Test> Tests { get; set; }
+  
+}
