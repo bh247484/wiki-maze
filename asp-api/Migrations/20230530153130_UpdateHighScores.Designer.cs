f7dada9251f0c9b08a2f56bb2e14ce20fa474f29
bh247484 <bh247484@gmail.com>
2023-05-30 11:33:48 -0400

    Update HighScores schema, generate migration

--- asp-api/Migrations/20230530153130_UpdateHighScores.Designer.cs
+++ asp-api/Migrations/20230530153130_UpdateHighScores.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using asp_net_app.Data;
@@ -11,9 +12,11 @@ using asp_net_app.Data;
 namespace asp_net_app.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230530153130_UpdateHighScores")]
+    partial class UpdateHighScores
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- asp-api/Models/HighScore.cs
+++ asp-api/Models/HighScore.cs
@@ -2,10 +2,10 @@ namespace asp_net_app.Models;
 
 public class HighScore 
 {
-  // Id as string so that 'Name' field can be used as primary key.
-  public string Id { get; set; }
-  public List<string> Path { get; set; }
-  public int Seconds { get; set; }
-  public int Strokes { get; set; }
+  public int Id { get; set; } = default!;
+  public string Name { get; set; } = default!;
+  public List<string> Path { get; set; } = default!;
+  public int Seconds { get; set; } = default!;
+  public int Strokes { get; set; } = default!;
 
 }
