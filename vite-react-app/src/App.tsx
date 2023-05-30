dc1067883e1e35d83d2be19dcd71ff2cc85f16b7
bh247484 <bh247484@gmail.com>
2023-05-30 14:49:15 -0400

    Create phase and comp to collect and display high scores

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import Setup from './components/setup';
 import Trial from './components/trial';
 import GameReport from './components/game-report';
+import HighScores from './components/high-scores';
 
 function App() {
   const [phase, setPhase] = useState<number>(1);
   const [poles, setPoles]= useState<string[]>([]);
   const [finalPath, setFinalPath] = useState<string[]>([]);
+  const [strokes, setStrokes] = useState<number>(0);
   const [time, setTime] = useState<number>(0);
 
   useInterval(
@@ -28,11 +30,23 @@ function App() {
               start={poles[0]}
               setFinalPath={setFinalPath}
               setPhase={setPhase}
+              setStrokes={setStrokes}
               time={time}
               goal={poles[1]}
             />
           ),
-          3: <GameReport time={time} finalPath={finalPath} />,
+          3: <GameReport
+                finalPath={finalPath}
+                setPhase={setPhase}
+                strokes={strokes}
+                time={time}
+              />,
+          4: <HighScores
+                setPhase={setPhase}
+                strokes={strokes}
+                time={time}
+                finalPath={finalPath}
+              />
         }[phase]
       }
     </div>
---
+++ vite-react-app/src/components/high-scores.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from 'react';
+
+interface IProps {
+  finalPath: string[];
+  setPhase: (phase: number) => void,
+  strokes: number;
+  time: number;
+}
+
+interface IHighScores {
+  id?: number;
+  name: string;
+  path: string[];
+  seconds: number;
+  strokes: number;
+}
+
+export default function HighScores({ finalPath, setPhase, strokes, time }: IProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [highScores, setHighScores] = useState<IHighScores[]>([]);
+  const [name, setName] = useState<string>('');
+  const [subPhase, setSubPhase] = useState<string>('');
+
+  // Fetch high scores from backend api.
+  async function fetchScores() {
+    try {
+      const response = await fetch('http://localhost:5000/HighScore');
+      const result = await response.json();
+      const sortedScores = result.sort((a: IHighScores, b: IHighScores) => a.strokes - b.strokes);
+      setHighScores(sortedScores);
+    } catch(error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function submitNewScore(name: string) {
+    // Update local, frontend high scores.
+    const updatedScores = [...highScores];
+    const newScore: IHighScores = {
+      name,
+      path: finalPath,
+      seconds: time,
+      strokes: strokes
+    }
+    if (updatedScores.length === 10) {
+      updatedScores.splice(-1);
+    }
+    updatedScores.push(newScore);
+    updatedScores.sort((a: IHighScores, b: IHighScores) => a.strokes - b.strokes);
+    setHighScores(updatedScores);
+
+    // Update backend db high scores.
+    try {
+      const response = await fetch('http://localhost:5000/HighScore', {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newScore),
+      });
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    } finally {
+      setSubPhase('show-scores');
+    }
+  }
+
+  // Init.
+  useEffect(() => {
+    fetchScores();
+  }, []);
+
+  // Check if new high score.
+  useEffect(() => {
+    if (!loading && highScores.length > 0) {
+      if (highScores.length < 10 || strokes < highScores[highScores.length - 1].strokes) {
+        setSubPhase('new-high-score');
+      } else {
+        setSubPhase('show-scores');
+      }
+    }
+  }, [highScores, loading, finalPath, strokes, time]);
+
+  if (loading) {
+    return (
+      <div className="high-scores-wrapper">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (subPhase === 'new-high-score') {
+    return(
+      <div className="high-scores-wrapper">
+        <h2>New High Score!</h2>
+          <p>Enter Your Name</p>
+          <input
+            maxLength={3}
+            type="text"
+            value={name}
+            onChange={({ target }) => setName(target.value.toUpperCase())}
+            onKeyUp={({ key }) => key === 'Enter' ? submitNewScore(name) : null}
+          />
+          <button onClick={() => submitNewScore(name)}>Submit</button>
+      </div>
+    );
+  }
+
+  if (subPhase === 'show-scores') {
+    return(
+      <div className="high-scores-wrapper">
+        {
+          highScores.map(({ name, path, seconds, strokes }) => (
+            <div className="score-wrapper">
+              <p>{name}</p>
+              <p>{path[0]} to {path[path.length - 1]}</p>
+              <p>{seconds}</p>
+              <p>{strokes}</p>
+            </div>
+          ))
+        }
+        <button onClick={() => setPhase(1)}>New Game</button>
+      </div>
+    );
+  }
+
+  // This will never happen.
+  // Need to have an unconditional, fallback return to appease Typescript.
+  return <></>;
+}
