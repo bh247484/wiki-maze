80c17a8b4767c851cca7d552921b0318ee33b3e2
bh247484 <bh247484@gmail.com>
2023-05-30 11:52:39 -0400

    Create HighScoreController with GET and POST logic

---
+++ asp-api/Controllers/HighScoreController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using asp_net_app.Data;
+using asp_net_app.Models;
+
+namespace asp_net_app.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HighScoreController : ControllerBase
+{
+  private readonly ApiDbContext _context;
+  public class ScoreParams {
+    public List<string> path { get; set; } = default!;
+    public string name { get; set; } = default!;
+    public int seconds { get; set; } = default!;
+  }
+
+  public HighScoreController(ApiDbContext context)
+  {
+      _context = context;
+  }
+
+  private int CalculateStrokes(int seconds, int pathLen) {
+    // Casting to int rounds down.
+    int timePenalty = (int)(seconds / 30);
+
+    return timePenalty + pathLen - 1;
+  }
+
+  [HttpPost(Name = "PostHighScore")]
+  public async Task<HighScore> Create(ScoreParams scoreParams)
+  {
+    var newHighScore = new HighScore()
+    {
+      Path = scoreParams.path,
+      Name = scoreParams.name,
+      Seconds = scoreParams.seconds,
+      Strokes = this.CalculateStrokes(scoreParams.seconds, scoreParams.path.Count)
+    };
+
+    // If there's already 10 high scores, remove the lowest score.
+    var allHighScores = await _context.HighScores.ToListAsync();
+    if (allHighScores.Count == 10) {
+      allHighScores.Sort((a,b) => a.Strokes - b.Strokes);
+      HighScore worstScore = allHighScores[allHighScores.Count - 1];
+      _context.HighScores.Remove(worstScore);
+    }
+
+    _context.HighScores.Add(newHighScore);
+    await _context.SaveChangesAsync();
+
+    return newHighScore;
+  }
+
+  [HttpGet(Name = "GetHighScore")]
+  public async Task<IActionResult> GetAll() {
+    var allHighScores = await _context.HighScores.ToListAsync();
+    return Ok(allHighScores);
+  }
+}
